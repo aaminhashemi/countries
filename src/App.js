c655e258e608a14e8baed729ca9fe845a2221a0c
Amin Hashemi <aaminhashemi74@gmail.com>
2023-02-26 14:48:23 +0330

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import Index from "./components/country/Index";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {BrowserRouter ,Routes,Route} from 'react-router-dom';
+import Detail from './components/country/Detail'
 function App() {
-  return (<Index />);
+
+    return (
+        <BrowserRouter >
+            <Routes>
+                <Route path='/' element={<Index/>} ></Route>
+                <Route path='/test' element={<Detail/>}></Route>
+            </Routes>
+
+        </BrowserRouter >
+            )
 }
 
 export default App;
---
+++ src/components/country/Detail.jsx
@@ -0,0 +1,8 @@
+const Detail=()=>{
+    return(
+        <h1>
+            Hello there
+        </h1>
+    )
+}
+export default Detail;
