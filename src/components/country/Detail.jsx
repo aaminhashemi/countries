093fe679f8535d65644885afce6d5f2b166fd441
aaminhashemi <aaminhashemi74@gmail.com>
2023-03-01 20:22:03 +0330

    finalizing project

--- src/components/country/Detail.jsx
+++ src/components/country/Detail.jsx
@@ -1,65 +1,115 @@
-import { useLocation } from "react-router-dom";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarText,
-  Form,
-  Row,
-  Button,
-  Col,
-  Input,
-  InputGroupText,
-  InputGroup,
-  CardTitle,
-  CardBody,
-  Card,
-  CardText,
-} from "reactstrap";
-import { useEffect, useState } from "react";
+import useDarkMode from "../useDarkMode";
+import { Link, useLocation } from "react-router-dom";
+import { Navbar, NavbarBrand, NavbarText } from "reactstrap";
 
 const Detail = () => {
-  const [mode, setMode] = useState("light");
-  const handleModeChange = () => {
-    setMode(mode === "light" ? "dark" : "light");
-  };
+  const [theme, toggleTheme] = useDarkMode();
+
   const location = useLocation();
   const data = location.state;
-  console.log(data);
   return (
     <>
-      <Navbar className="mb-2" color={mode}>
-        <NavbarBrand className={mode === "light" ? "text-dark" : "text-light"}>
+      <Navbar
+        className={
+          theme === "light"
+            ? "shadow-sm p-3 light-mode"
+            : "shadow-sm p-3 input-and-nav-dark"
+        }
+      >
+        <NavbarBrand className={theme === "light" ? "light-text" : "dark-text"}>
           Where in the world
         </NavbarBrand>
         <NavbarText
-          className="fa fa-moon-o"
-          color={mode === "light" ? "dark" : "light"}
+          className={
+            theme === "light"
+              ? "fa fa-moon-o light-text"
+              : "fa-moon-o dark-text fa "
+          }
         >
           <span
-            onClick={handleModeChange}
-            className={mode === "light" ? "text-dark" : "text-light"}
+            onClick={toggleTheme}
           >
-            {mode === "light" ? "Dark" : "Light"} Mode
+            {theme === "light" ? " Dark " : " Light "} Mode
           </span>
         </NavbarText>
       </Navbar>
-      <div className="container-fluid px-5 d-flex flex-row flex-wrap justify-content-center mt-5">
-        <img src={data.flag} width='250px' className="mx-3"/>
-        <div className="d-flex flex-row flex-wrap align-items-center">
-            <div className="d-flex flex-column flex-wrap ">
+      <div className="d-flex flex-column h-100">
+        <div
+          className={
+            theme === "light"
+              ? "d-flex pt-5 pb-5 justify-content-start light-mode"
+              : "d-flex pt-5 pb-5 justify-content-start dark-mode"
+          }
+        >
+          <div
+            className={
+              theme === "light"
+                ? "col-lg-1 text-center py-2 rounded mx-5 input-and-nav-light"
+                : "col-lg-1 text-center py-2 rounded mx-5 input-and-nav-dark"
+            }
+          >
+            <Link
+              to={"/"}
+              className={
+                theme === "light"
+                  ? "button button-dark light-text text-decoration-none"
+                  : "button button-dark dark-text text-decoration-none"
+              }
+            >
+              <span className="fa fa-arrow-left"></span> Back
+            </Link>
+          </div>
+        </div>
+        <div id="parent-element" className="h-100 d-inline">
+          <div
+            className={
+              theme === "light"
+                ? "d-flex flex-row flex-wrap h-100 container-fluid px-5  justify-content-evenly pt-5 pb-5 light-mode align-items-center"
+                : "d-flex flex-row flex-wrap  h-100 container-fluid px-5  justify-content-evenly pt-5 pb-5 dark-mode align-items-center"
+            }
+          >
+            <img src={data.flag} width="400px" height='300px' className="mx-3" />
+            <div className="d-flex flex-row  flex-wrap align-items-center">
+              <div className="d-flex flex-column flex-wrap ">
                 <h1>{data.name}</h1>
-                <p>{`Nativ Name : ${data.nativeName}`}</p>
-                <p>{`Population : ${data.population}`}</p>
-                <p>{`Region : ${data.region}`}</p>
-                <p>{`Sub Region : ${data.subregion}`}</p>
-                <p>{`Capital : ${data.capital}`}</p>
+                <p>
+                  <b>Nativ Name :</b>
+                  {` ${data.nativeName}`}
+                </p>
+                <p>
+                  <b>Population :</b>
+                  {` ${data.population.toLocaleString("en-US")}`}
+                </p>
+                <p>
+                  <b>Region : </b>
+                  {`${data.region}`}
+                </p>
+                <p>
+                  <b>Sub Region :</b>
+                  {` ${data.subregion}`}
+                </p>
+                <p>
+                  <b>Capital :</b>
+                  {` ${data.capital}`}
+                </p>
+              </div>
+              <div className="d-flex flex-column flex-wrap ">
+                <p>
+                  <b>Top Level Domain :</b>
+                  {` ${data.topLevelDomain}`}
+                </p>
+                <p>
+                  <b>Currencies :</b>
+                  {` ${data.currencies?.map((item) => item.name)}`}
+                </p>
+                <p>
+                  <b>Languages :</b>
+                  {` ${data.languages?.map((item) => item.name)}`}
+                </p>
+              </div>
+              <div></div>
             </div>
-            <div className="d-flex flex-column flex-wrap ">
-                <p>{`Top Level Domain : ${data.topLevelDomain}`}</p>
-                <p>{`Currencies : ${data.currencies}`}</p>
-                <p>{`Languages : ${data.languages.map(item=>item.name)}`}</p>
-            </div>
-            <div></div>
+          </div>
         </div>
       </div>
     </>
--- src/components/country/Index.jsx
+++ src/components/country/Index.jsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import useDarkMode from "../useDarkMode";
 import { Link } from "react-router-dom";
 import {
   Navbar,
@@ -6,20 +7,16 @@ import {
   NavbarText,
   Form,
   Row,
-  Button,
   Col,
   Input,
-  InputGroupText,
-  InputGroup,
   CardTitle,
   CardBody,
   Card,
-  CardText,
 } from "reactstrap";
 import { useEffect, useState } from "react";
 
 const Index = () => {
-  const [mode, setMode] = useState("light");
+  const [theme, toggleTheme] = useDarkMode();
 
   const [countries, setCountries] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
@@ -38,52 +35,86 @@ const Index = () => {
         setFilteredCountries(myJson);
       });
   }, []);
-  const handleModeChange = () => {
-    setMode(mode === "light" ? "dark" : "light");
-  };
 
   const filterCountries = (e) => {
     setFilteredCountries(
-      countries.filter((item) => item.region === e.target.value)
-    );
+      countries.filter((item) => 
+      item.region === e.target.value)
+
+      )
+  }
+
+  const filterNameCountry = (e) => {
+    setFilteredCountries(
+        filteredCountries.filter((item) => 
+      item.name.toLowerCase().includes(e.target.value.toLowerCase())
+
+    ))
   };
   return (
     <>
-      <Navbar className="mb-2" color={mode}>
-        <NavbarBrand className={mode === "light" ? "text-dark" : "text-light"}>
+      <Navbar
+        className={
+          theme === "light"
+            ? "shadow-sm p-3 light-mode"
+            : "shadow-sm p-3 input-and-nav-dark"
+        }
+      >
+        <NavbarBrand className={theme === "light" ? "light-text" : "dark-text"}>
           Where in the world
         </NavbarBrand>
         <NavbarText
-          className="fa fa-moon-o"
-          color={mode === "light" ? "dark" : "light"}
+          className={
+            theme === "light"
+            ? "fa fa-moon-o light-text"
+            : "fa-moon-o dark-text fa "
+          }
         >
           <span
-            onClick={handleModeChange}
-            className={mode === "light" ? "text-dark" : "text-light"}
+            onClick={toggleTheme}
+            className={theme === "light" ? "light-text" : "dark-text"}
           >
-            {mode === "light" ? "Dark" : "Light"} Mode
+            {theme === "light" ? " Dark " : " Light "} Mode
           </span>
         </NavbarText>
       </Navbar>
-      <div className="container-fluid px-5">
-        <Form className="">
+      <div
+        className={
+          theme === "light"
+            ? "container-fluid px-5 light-mode"
+            : "container-fluid px-5 dark-mode"
+        }
+      >
+        <Form className="pt-3">
           <Row className="d-flex flex-wrap justify-content-between">
-            <Col className="col-lg-3 col-xs-12 col-sm-12 col-md-3 pb-3">
-              <InputGroup>
-                <InputGroupText className="bg-light" color="danger">
-                  <i className="fa fa-search"></i>
-                </InputGroupText>
-                <Input
-                  name="country"
-                  placeholder="search for a country"
-                  type="text"
+            <Col className="col-lg-4 col-xs-12 col-sm-12 col-md-3 pb-3">
+              <div className="group">
+                <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
+                  <g>
+                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                  </g>
+                </svg>
+                <input
+                  placeholder="search for a country..."
+                  type="search"
+                  onChange={filterNameCountry}
+                  className={
+                    theme === "light"
+                      ? "input text-dark input-and-nav-light"
+                      : "input text-light input-and-nav-dark"
+                  }
                 />
-              </InputGroup>
+              </div>
             </Col>
-            <Col className="col-lg-3 col-xs-6 col-md-3 col-sm-6  pb-3">
+            <Col className="col-lg-2 col-xs-6 col-md-2 col-sm-6  pb-3">
               <Input
                 id="exampleSelect"
                 name="select"
+                className={
+                  theme === "light"
+                    ? "rounded select text-dark input-and-nav-light"
+                    : "rounded select text-light input-and-nav-dark"
+                }
                 type="select"
                 onChange={filterCountries}
               >
@@ -100,24 +131,38 @@ const Index = () => {
           </Row>
         </Form>
       </div>
-      <div className="container-fluid d-flex flex-row flex-wrap justify-content-between">
+      <div
+        className={
+          theme === "light"
+            ? "container-fluid px-5 d-flex flex-row flex-wrap justify-content-center light-mode"
+            : "container-fluid px-5 d-flex flex-row flex-wrap justify-content-center dark-mode"
+        }
+      >
         {filteredCountries?.map((country, index) => (
           <Card
             key={index}
             style={{
-              width: "18rem",
+              width: "15rem",
             }}
-            className="my-3 shadow-sm"
+            className="my-3 mx-3 shadow-sm"
           >
-            <img alt="Sample" src={country.flags.svg} />
-            <CardBody>
+            <Link to={"/test"} state={country}>
+              <img
+                alt="Sample "
+                className="card-img-top"
+                src={country.flags.svg}
+                height="140px"
+                style={{ objectFit: "cover" }}
+              />
+            </Link>
+            <CardBody
+              className={theme === "light" ? "light-mode" : "dark-mode"}
+            >
               <CardTitle tag="h5">{country.name}</CardTitle>
-              <Link to={"/test"} state={country}>
-                test
-              </Link>
+
               <span className="d-block">
                 <b>Population: </b>
-                {` ${country.population}`}
+                {` ${country.population.toLocaleString("en-US")}`}
               </span>
               <span className="d-block">
                 <b>Region: </b>
